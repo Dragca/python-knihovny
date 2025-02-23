a0957cd4781bb6df3b08e1408250418860beb71d
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-02-23 23:21:47 +0100

    ukol promitani

--- 01/promitani.py
+++ 01/promitani.py
@@ -1,4 +1,14 @@
 # https://kodim.cz/czechitas/uvod-do-progr-2/bonusy/cykly-2/list-comprehension/promitani
 
 def prevod(vstup):
-    ...
+    trvani = [prevod_na_retezec(x) for x in vstup]
+    return trvani
+
+
+def prevod_na_retezec(stopaz):
+    hodin = stopaz // 60
+    minut = stopaz % 60
+    return f"{hodin}:{minut:0>2}"
+
+
+# print(prevod((136, 105, 82)))
