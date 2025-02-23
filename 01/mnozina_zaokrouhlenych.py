fc4da9faa62eeb8a75324addf3cf28287b87a6e5
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-02-23 22:54:24 +0100

    vyuzito list comprehension

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -1,8 +1,11 @@
 # Napiš funkci, která vrátí ze iterovatelného vstupu množinu zaokrouhlených hodnot
 
-def mnozina_zaokrouhlenych(vstup):
-    mnozina = set()
-    for prvek in vstup:
-        mnozina.add(round(prvek))
+# def mnozina_zaokrouhlenych(vstup):
+#     mnozina = set()
+#     for prvek in vstup:
+#         mnozina.add(round(prvek))
 
-    return mnozina
+#     return mnozina
+
+def mnozina_zaokrouhlenych(vstup):
+    return {round(prvek) for prvek in vstup}
