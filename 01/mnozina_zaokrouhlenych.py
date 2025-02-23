fc30fe09135935259bd98e19222af0580a2e5a32
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-02-23 21:45:55 +0100

    ukol mnozina

--- 01/mnozina_zaokrouhlenych.py
+++ 01/mnozina_zaokrouhlenych.py
@@ -1,4 +1,8 @@
 # Napiš funkci, která vrátí ze iterovatelného vstupu množinu zaokrouhlených hodnot
 
 def mnozina_zaokrouhlenych(vstup):
-    ...
+    mnozina = set()
+    for prvek in vstup:
+        mnozina.add(round(prvek))
+
+    return mnozina
