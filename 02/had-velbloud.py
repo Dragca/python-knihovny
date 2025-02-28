1f403ba072159dd6369a605126b78489582b188c
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-02-28 13:56:21 +0100

    upraven ukol had_velbloud s vyuzitim retzcových funkcí

--- 02/had-velbloud.py
+++ 02/had-velbloud.py
@@ -8,18 +8,8 @@ import sys
 if len(sys.argv) != 2:
     exit(f"Usage:{sys.argv[0]} <rezetezec>")
 
-
-def change_string(string):
-    prev_char = ''
-    new_string = ''
-
-    for char in string:
-        if prev_char == '_':
-            char = char.upper()
-        prev_char = char
-        if char != '_':
-            new_string += char
-
-    return new_string
-
-print(change_string(sys.argv[1]))
+# modified using string functions
+snake = sys.argv[1]
+words = snake.split('_')  # splits a string into individual words based on a delimiter and returns a list of words
+# print([slovo.capitalize() for slovo in words[1:]]) - tried list comprehension
+print(words[0] + "".join(w.capitalize() for w in words[1:]))
