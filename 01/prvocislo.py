3f8d340c738e461146442c5e23899692061bc5de
Dragca Kvasnickova <kvasnickova@kvadr.eu>
2025-03-11 12:34:41 +0100

    upraven ukol prvočíslo

--- 01/prvocislo.py
+++ 01/prvocislo.py
@@ -1,17 +1,23 @@
 # Požádej uživatele o zadání celého čísla. Následně urči, zda je toto číslo prvočíslo.
+# pro výpočet druhé odmocniny lze použít funkci sqrt z modulu math nebo n ** 0.5
 
-try:
-    cislo = int(input("Vlož číslo: "))
-# delitelne = []# nejdriv jsem si to musela rozepsat takto 
-# for i in range(1,cislo + 1):
-#     if cislo % i == 0:
-#         delitelne.append(i)
-# upraveno pomocí list comprehension
-    delitelne = [i for i in range(1, cislo + 1)if cislo % i == 0]
-    if len(delitelne) == 2:
-        print('zadané číslo je prvočíslo')
+from math import sqrt
+
+while True:
+    try:
+        cislo = int(input("Vlož číslo: ")) # ošetřit, že jde převod na celé číslo
+        break
+    except ValueError:
+        print('Je třeba zadat celé číslo.')
+
+if cislo < 2:# čísla menší než 2 nejsou prvočísla
+    print('Toto není prvočíslo')
+
+else:
+    for i in range(2,int(sqrt(cislo))+1):
+        if cislo % i == 0:
+            print('Zadané číslo není prvočíslo')
+            break
+
     else:
-        print('zadané číslo není prvočíslo')
-
-except ValueError:
-    print('Je třeba zadat celé číslo.')
+        print('Zadané číslo je prvočíslo')
